7fea751b0a9f708ca23eb9b2d370c34d49c13bf5
Matteralmar <ultramacflaw@gmail.com>
2021-05-24 12:53:33 +0200

    cache commit

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -97,7 +97,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+CACHE = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211'
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
