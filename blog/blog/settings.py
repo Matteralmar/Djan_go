52aaede6e3d421f9889fc9c0393d2bfca75f4371
Matteralmar <ultramacflaw@gmail.com>
2021-05-24 11:03:24 +0200

    cache commit2

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -98,13 +98,6 @@ DATABASES = {
     }
 }
 
-CACHE = {
-    'default': {
-        'BACKEND': 'django.core.cache',
-        'LOCATION': '127:0.0.1:11211'
-    }
-
-}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
